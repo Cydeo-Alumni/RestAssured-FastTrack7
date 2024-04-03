0482a380c19e7867126c7ab1e7e7648a3866bf72
mehmetCt <mehmet@cybertekschool.com>
2024-04-03 16:01:10 -0400

    Project Init

---
+++ src/test/java/com/cydeo/P01_Intro.java
@@ -0,0 +1,12 @@
+package com.cydeo;
+
+import org.junit.jupiter.api.Test;
+
+public class P01_Intro {
+
+    @Test
+   public  void intro() {
+
+        System.out.println("This is intro for API FastTrack");
+    }
+}
