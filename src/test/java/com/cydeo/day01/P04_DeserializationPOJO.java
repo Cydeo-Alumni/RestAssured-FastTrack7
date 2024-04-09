804f28fb48ec095f8514926da995e51da0954cc0
mehmetCt <mehmet@cybertekschool.com>
2024-04-09 15:41:50 -0400

    Day 01 Deserialization

---
+++ src/test/java/com/cydeo/day01/P04_DeserializationPOJO.java
@@ -0,0 +1,35 @@
+package com.cydeo.day01;
+
+import com.cydeo.utility.FakeStoreTestBase;
+import org.junit.jupiter.api.Test;
+
+public class P04_DeserializationPOJO extends FakeStoreTestBase {
+
+    /**
+     * Send request to FakeStoreTestBase url and save the response
+     * Accept application/json
+     * Path Param id is 2
+     * Query Param limit 10
+     * Query Param offset 0
+     * GET /categories/{id}/products
+     * Store the response in Response Object that comes from get Request
+     * Print out followings
+     * - Print response
+     * - Content-Type is application/json
+     * - Status Code is 200
+     * - Retrieve data as JAVA Collections and print out following information
+     *
+     * System.out.println("====== GET ALL PRODUCTS ======");
+     * System.out.println("====== GET SECOND PRODUCTS ======");
+     * System.out.println("====== GET SECOND PRODUCTS PRICE ======");
+     * System.out.println("====== GET SECOND PRODUCTS IMAGES ======");
+     * System.out.println("====== GET SECOND PRODUCTS FIRST IMAGE ======");
+     * System.out.println("====== GET SECOND PRODUCTS CATEGORY ======");
+     * System.out.println("====== GET SECOND PRODUCTS CATEGORY NAME ======");
+     */
+
+    @Test
+    public void task1() {
+
+    }
+}
