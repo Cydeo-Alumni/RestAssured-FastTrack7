c834e97f4bebcaa0ff6171f0ba64a2085016f7fb
mehmetCt <mehmet@cybertekschool.com>
2024-04-09 15:27:45 -0400

    Day 01 HW Query Param

---
+++ src/test/java/com/cydeo/day01/P03_QueryParam.java
@@ -0,0 +1,19 @@
+package com.cydeo.day01;
+
+public class P03_QueryParam {
+    /**
+     * 1- Given accept type is Json
+     * 2- Query Parameters value is
+     * - limit —> 0
+     * - offset —> 100
+     * 3- When user sends GET request to /products
+     * 4- Verify followings
+     * - Status code should be 200
+     * - Content Type is application/json; charset=utf-8
+     * - Each product has id
+     * - Each product has category id
+     * - Get all product names
+     * - Get product ids
+     * - Get all category names
+     */
+}
