dc8f2c6956dce15626e8b0a39172235ce9653378
mehmetCt <mehmet@cybertekschool.com>
2024-04-09 16:21:58 -0400

    Day 01 POJO

--- src/test/java/com/cydeo/day01/P04_DeserializationPOJO.java
+++ src/test/java/com/cydeo/day01/P04_DeserializationPOJO.java
@@ -1,10 +1,18 @@
 package com.cydeo.day01;
 
+import com.cydeo.pojo.Product;
 import com.cydeo.utility.FakeStoreTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+
 public class P04_DeserializationPOJO extends FakeStoreTestBase {
 
     /**
@@ -17,10 +25,10 @@ public class P04_DeserializationPOJO extends FakeStoreTestBase {
      * Store the response in Response Object that comes from get Request
      * Print out followings
      * - Print response
-     * - Content-Type is application/json
+     * - Content-Type is application/json; charset=utf-8
      * - Status Code is 200
      * - Retrieve data as JAVA Collections and print out following information
-     *
+     * <p>
      * System.out.println("====== GET ALL PRODUCTS ======");
      * System.out.println("====== GET SECOND PRODUCTS ======");
      * System.out.println("====== GET SECOND PRODUCTS PRICE ======");
@@ -33,8 +41,34 @@ public class P04_DeserializationPOJO extends FakeStoreTestBase {
     @Test
     public void task1() {
 
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", 2)
+                .queryParam("limit", 10)
+                .queryParam("offset", 0).
+                when().get("/categories/{id}/products").
+                then().
+                statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .extract().response();
+
+        // We will start DESERIALIZATION
+
+        JsonPath jp = response.jsonPath();
+
+        List<Product> allProducts = jp.getList("", Product.class);
 
 
+        System.out.println("====== GET ALL PRODUCTS ======");
+        for (Product eachProduct : allProducts) {
+            System.out.println("eachProduct = " + eachProduct);
+        }
+
+        System.out.println("====== GET SECOND PRODUCTS ======");
+        System.out.println("====== GET SECOND PRODUCTS PRICE ======");
+        System.out.println("====== GET SECOND PRODUCTS IMAGES ======");
+        System.out.println("====== GET SECOND PRODUCTS FIRST IMAGE ======");
+        System.out.println("====== GET SECOND PRODUCTS CATEGORY ======");
+        System.out.println("====== GET SECOND PRODUCTS CATEGORY NAME ======");
 
 
     }
--- src/test/java/com/cydeo/pojo/Product.java
+++ src/test/java/com/cydeo/pojo/Product.java
@@ -1,5 +1,7 @@
 package com.cydeo.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 
@@ -7,12 +9,14 @@ import lombok.Data;
 import java.util.List;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Product {
 
     private int id;
     private String title;
     private int price;
 
+    @JsonProperty("images")
     private List<String> allImages;
 
     private Category category;
