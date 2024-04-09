e8a8639ce66b1335a4f6b605558472b9d0cd222a
mehmetCt <mehmet@cybertekschool.com>
2024-04-09 14:53:33 -0400

    Day 01

--- src/test/java/com/cydeo/day01/P02_PathParam.java
+++ src/test/java/com/cydeo/day01/P02_PathParam.java
@@ -3,28 +3,31 @@ package com.cydeo.day01;
 import com.cydeo.utility.FakeStoreTestBase;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class P02_PathParam extends FakeStoreTestBase {
     /**
-     *1- Given accept type is Json
-     *2- Path Parameters value is
-     *     - id —> 13
-     *3- When user sends GET request to api/v1/products/{id}
-     *4- Verify followings
-     *     - Status code should be 200
-     *     - Content Type is application/json; charset=utf-8
-     *     - Print response
-     *     - id is 13
-     *     - Name is "Classic Olive Chino Shorts"
-     *     - Category name is "Clothes"
+     * 1- Given accept type is Json
+     * 2- Path Parameters value is
+     * - id —> 13
+     * 3- When user sends GET request to api/v1/products/{id}
+     * 4- Verify followings
+     * - Status code should be 200
+     * - Content Type is application/json; charset=utf-8
+     * - Print response
+     * - id is 13
+     * - Name is "Classic Olive Chino Shorts"
+     * - Category name is "Clothes"
      */
     @Test
     public void responsePath() {
 
-        Response response = RestAssured.given().accept(ContentType.JSON)
+        Response response = given().accept(ContentType.JSON)
                 .pathParam("id", 13)
                 .when().get("/products/{id}");
 
@@ -33,21 +36,21 @@ public class P02_PathParam extends FakeStoreTestBase {
 
         //     *     - Status code should be 200
 
-        Assertions.assertEquals(200,response.statusCode());
+        assertEquals(200, response.statusCode());
 
         //     *     - Content Type is application/json; charset=utf-8
 
-        Assertions.assertEquals("application/json; charset=utf-8",response.contentType());
+        assertEquals("application/json; charset=utf-8", response.contentType());
 
         //     *     - id is 13
         int id = response.path("id");
-        Assertions.assertEquals(13,id);
+        assertEquals(13, id);
 
         //     *     - Name is "Classic Olive Chino Shorts"
-        Assertions.assertEquals("Classic Olive Chino Shorts",response.path("title"));
+        assertEquals("Classic Olive Chino Shorts", response.path("title"));
 
         //     *     - Category name is "Clothes"
-        Assertions.assertEquals("Clothes",response.path("category.name"));
+        assertEquals("Clothes", response.path("category.name"));
 
 
         // Verify category.imageurl link is working
@@ -55,7 +58,40 @@ public class P02_PathParam extends FakeStoreTestBase {
         System.out.println("imgUrl = " + imgUrl);
 
         int statusCode = RestAssured.get(imgUrl).statusCode();
-        Assertions.assertEquals(200,statusCode);
+        assertEquals(200, statusCode);
+
+    }
+
+
+    @Test
+    public void jsonPath() {
+
+        Response response = given().accept(ContentType.JSON)
+                .pathParam("id", 13)
+                .when().get("/products/{id}");
+
+
+        //     *     - Print response
+       response.prettyPrint();
+
+        //     *     - Status code should be 200
+        assertEquals(200,response.statusCode());
+
+        //     *     - Content Type is application/json; charset=utf-8
+        assertEquals("application/json; charset=utf-8",response.contentType());
+
+
+        // CREATE A JSONPATH OBJECT
+        JsonPath jp = response.jsonPath();
+        //     *     - id is 13
+        int id = jp.getInt("id");
+        assertEquals(13,id);
+
+        //     *     - Name is "Classic Olive Chino Shorts"
+        assertEquals("Classic Olive Chino Shorts",jp.getString("title"));
+
+        //     *     - Category name is "Clothes"
+        assertEquals("Clothes",jp.getString("category.name"));
 
     }
 
