cbbf6e365fb6e7bfb98125872cb4c2afc8abefc6
mehmetCt <mehmet@cybertekschool.com>
2024-04-11 15:48:45 -0400

    Day 02 Serialization File

---
+++ src/test/java/com/cydeo/day02/P02_SerializationJsonFile.java
@@ -0,0 +1,13 @@
+package com.cydeo.day02;
+
+import com.cydeo.utility.FakeStoreTestBase;
+import org.junit.jupiter.api.Test;
+
+public class P02_SerializationJsonFile extends FakeStoreTestBase {
+
+
+    @Test
+    public void task1() {
+
+    }
+}
