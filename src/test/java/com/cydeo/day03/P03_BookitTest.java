91cd1a9089405046ae1c82479a7caac3df4b29cc
mehmetCt <mehmet@cybertekschool.com>
2024-04-16 14:17:49 -0400

    Day 04 Log4j2

--- src/test/java/com/cydeo/day03/P03_BookitTest.java
+++ src/test/java/com/cydeo/day03/P03_BookitTest.java
@@ -1,10 +1,15 @@
 package com.cydeo.day03;
 
+import com.cydeo.pojo.Campus;
+import com.cydeo.pojo.Cluster;
+import com.cydeo.pojo.Room;
 import com.cydeo.utility.BookitTestBase;
 import com.cydeo.utility.BookitUtil;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static io.restassured.RestAssured.*;
 
@@ -31,7 +36,7 @@ public class P03_BookitTest extends BookitTestBase {
     public void task1() {
 
         // Get Token
-        String email=System.getenv("B_USERNAME");
+        String email=System.getenv("B_EMAIL");
         System.out.println("email = " + email);
         String password=System.getenv("B_PASS");
         System.out.println("password = " + password);
@@ -46,14 +51,24 @@ public class P03_BookitTest extends BookitTestBase {
                 then().statusCode(200)
                 .extract().jsonPath();
 
-        /*
-            JSON - POJO
+        List<Campus> allCampuses = jp.getList("", Campus.class);
+        for (Campus eachCampus : allCampuses) {
+            System.out.println("eachCampus = " + eachCampus);
+        }
 
-            //Find out how many room  we have in light-side
-            //Find out how many cluster we have in VA
-            //Find out how many campus we have
+        //Find out how many campus we have
+        System.out.println("allCampuses.size() = " + allCampuses.size());
 
-         */
+        //Find out how many cluster we have in VA
+        Campus firstCampus = allCampuses.get(0);
+        List<Cluster> allClusterInFirst = firstCampus.getClusterList();
+        System.out.println("allClusterInFirst.size() = " + allClusterInFirst.size());
+
+        //Find out how many room  we have in light-side
+        Cluster firstCluster = allClusterInFirst.get(0);
+        List<Room> allRooms = firstCluster.getRoomList();
+        System.out.println("allRooms.size() = " + allRooms.size());
+
 
     }
 
--- src/test/java/com/cydeo/pojo/Campus.java
+++ src/test/java/com/cydeo/pojo/Campus.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import java.util.List;
 
+
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Campus {
